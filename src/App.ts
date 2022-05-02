cf6c14d31ab44dfa529a345c2bbe759abaa9935b
Alexey Valko <valko.alexey@gmail.com>
2022-05-02 12:28:40 +0300

    refactor: replace createContainer method

--- src/App.ts
+++ src/App.ts
@@ -1,23 +1,14 @@
+import { createHtmlElement } from './utils/createHtmlElement';
 import { AudioPlayer } from './components/AudioPlayer/AudioPlayer';
 import { requestPlayList } from './utils/requestPlayList';
 
 export class App {
   private rootElement: Element;
-
-  container: HTMLDivElement;
-
-  player: AudioPlayer;
+  private container = createHtmlElement('div', 'app-container');
+  private player: AudioPlayer = new AudioPlayer();
 
   constructor(rootElement: Element) {
     this.rootElement = rootElement;
-    this.player = new AudioPlayer();
-    this.container = this.createContainer();
-  }
-
-  createContainer() {
-    const element = document.createElement('div');
-    element.classList.add('app-container');
-    return element;
   }
 
   async render() {
